4bf1a29fae0d011e8be87c837499932eb61a2eeb
vutran <vutran1412@icloud.com>
2018-09-12 17:26:08 -0500

    Completed the hand analyzer class

--- Player.py
+++ Player.py
@@ -1,23 +1,48 @@
+from Deck import *
+
+
+# Player class, holds player name, chips and hand
 class Player(object):
     def __init__(self):
         self.name = self.get_player_name()
         self.credits = self.add_chips()
         self.hand = []
 
+    # Function to store player name
     def get_player_name(self):
         self.name = input("What is your name?\n")
         return self.name
 
+    # Function to store player chips
     def add_chips(self):
         self.credits = int(input("How much money would you like to play with?\n"))
         return self.credits
 
+    # Function to store player information
     def __str__(self):
         return "Player: " + self.name + "\tCredits: " + str(self.credits)
 
+    # Show player hand
+    def show_hand(self):
+        for i in self.hand:
+            print(i, end=" ")
+
+    # Add cards to hand
+    def add_cards(self, cards):
+        return self.hand.append(cards)
+
+    # Places a bet
     def place_bet(self):
-        bet = int(input("How much would you like to bet?\n"))
+        bet = 5
         return self.credits - bet
 
+    # Adds winnings to current player's chips
     def winnings(self, winnings):
         return self.credits + winnings
+
+    # Count all the cards in player's hands
+    def card_count(self):
+        return len(self.hand)
+
+
+
